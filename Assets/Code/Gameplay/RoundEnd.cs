151e17f611aa93bd5c49226b5ca73c232df062ea
Dragvid <55921056+Dragvid@users.noreply.github.com>
2021-01-04 22:39:36 +0000

    atualizção programação

---
+++ Assets/Code/Gameplay/RoundEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundEnd : MonoBehaviour
+{
+    public GameObject[] line1;
+    public GameObject[] line2;
+    public GameObject[] line3;
+    public GameObject[] line4;
+    public GameObject[] line5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
